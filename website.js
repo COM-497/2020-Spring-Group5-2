6ea39001709d6768e72959af199d911d9b15f5d7
brianayamamoto <36461368+brianayamamoto@users.noreply.github.com>
2020-03-02 14:32:09 -0500

    Logo Update

--- website.js
+++ website.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-  
+
 var path = __dirname + '/views/';
   
 app.use('/',router);
+app.use(express.static('views'));
   
 router.get('/',function(req, res){
   res.sendFile(path + 'index.html');
 });
-  
+
 router.get('/about',function(req, res){
   res.sendFile(path + 'about.html');
 });
@@ -29,7 +30,8 @@ router.get('/support',function(req, res){
 app.use('*',function(req, res){
   res.send('Error 404: Not Found!');
 });
-  
+
+
 app.listen(3000,function(){
   console.log("Server running at Port 3000");
 });
