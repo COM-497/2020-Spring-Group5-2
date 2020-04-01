b9489b790013716e7ea3062e7320a9e98e6d92ad
brianayamamoto <36461368+brianayamamoto@users.noreply.github.com>
2020-04-01 11:09:35 -0400

    Connect to database and create user

--- website.js
+++ website.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+const {MongoClient} = require('mongodb');
 
 var path = __dirname + '/views/';
   
@@ -51,3 +52,33 @@ app.use('*',function(req, res){
 app.listen(3000,function(){
   console.log("Server running at Port 3000");
 });
+
+async function main() {   
+  const uri = "mongodb+srv://brianay:charinforg@mycluster-iw4qo.mongodb.net/test?retryWrites=true&w=majority";   
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });       
+
+  try {     
+    await client.connect();   
+    
+    await createUser(
+      client,
+      {
+        firstName: "user1first",
+        lastName: "user1last",
+        email: "test@email.com",
+        password: "test"
+      }
+    );
+  } 
+  catch (e) {
+    console.error(e);   
+  } finally {     
+    await client.close();   
+    }   
+}
+ main().catch(console.error);  
+          
+ async function createUser(client, newUser){
+  const result = await client.db("charinforg").collection("user").insertOne(newUser);
+  console.log(`New user created with the following id: ${result.insertedId}`);
+}
