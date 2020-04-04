9ec755c456cc0ab0e3b667a3d9cd092f567b545c
brianayamamoto <36461368+brianayamamoto@users.noreply.github.com>
2020-04-04 14:58:11 -0400

    event form save to database

--- website.js
+++ website.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var passport = require('passport')
+LocalStrategy = require('passport-local').Strategy;
 const {MongoClient} = require('mongodb');
 const {parse} = require('querystring');
 
@@ -33,6 +35,11 @@ router.get('/login',function(req, res){
   res.sendFile(path + 'login.html');
 });
 
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/myprofile',
+                                   failureRedirect: '/' })
+);
+
 router.get('/orgsignup',function(req, res){
   res.sendFile(path + 'orgsignup.html');
 });
@@ -58,6 +65,26 @@ req.on('end', () => {
       res.send('Passwords do not match');
     }
   
+});  
+});
+
+router.get('/eventForm',function(req, res){
+  res.sendFile(path + 'eventForm.html');
+});
+
+router.post('/eventForm', (req, res) => {   
+  let body = '';     
+  req.on('data', chunk => {         
+    body += chunk.toString(); // convert Buffer to string
+});
+
+req.on('end', () => {
+  var value = parse(body);
+      var queryString = require('querystring');
+      var event = queryString.parse('&organization='+value['organization']+'event='+value['event']+'dateOfEvent='+value['date']+'&descriptionEvent='+value['descriptionEvent']+'&typeOfEvent='+value['type']+'&descriptionOfOrg='+value['descriptionOrg']+'&contactToDonors='+value['contact']+'&goal='+value['goal']+'&whereMoneyGoes='+value['whereMoneyGoes']);
+      insertEvent(event);
+      console.log(event);
+      res.redirect('/events') 
 });  
 });
 
@@ -138,6 +165,24 @@ async function insertUser(newUser) {
   }
 }  
 
+async function insertEvent(newEvent) {
+  const uri = "mongodb+srv://brianay:charinforg@mycluster-iw4qo.mongodb.net/test?retryWrites=true&w=majority";   
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });       
+
+  try {     
+    await client.connect();   
+
+    const result = await client.db("charinforg").collection("event").insertOne(newEvent);
+  console.log(`${result.insertedCount} new event created with the following id: `);
+  console.log(result.insertedId);
+    }
+  catch (e) {
+    console.error(e);   
+} finally {     
+    await client.close();   
+  }
+}  
+
 /*async function main() {   
   const uri = "mongodb+srv://brianay:charinforg@mycluster-iw4qo.mongodb.net/test?retryWrites=true&w=majority";   
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });       
@@ -195,3 +240,20 @@ async function insertUser(newUser) {
   console.log(`New user created with the following id: ${result.insertedId}`);
 }
 */
+
+passport.use(new LocalStrategy({
+  usernameField: 'email'
+},
+  function(username, password, done) {
+    User.findOne({ email: email }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
