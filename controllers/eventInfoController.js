31b9ee156e509fa9880b5caeaf9257a6315c29e0
brianayamamoto <36461368+brianayamamoto@users.noreply.github.com>
2020-04-17 21:43:51 -0400

    delete controllers

--- controllers/eventInfoController.js
+++
@@ -1,9 +0,0 @@
-const Event = require('../models/Event');
-
-exports.getOneEvent = (req, res, next) => {
-    Event.find( {'eventName': 'Virual 5k'}, (error, event) => {
-      if (error) next(error);
-      req.data = event;
-      next();
-    });
-  };
--- controllers/eventsController.js
+++
@@ -1,9 +0,0 @@
-const Event = require('../models/Event');
-
-exports.getAllEvents = (req, res, next) => {
-    Event.find( {}, (error, events) => {
-      if (error) next(error);
-      req.data = events;
-      next();
-    });
-  };
