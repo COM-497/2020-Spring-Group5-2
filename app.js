55381780572bc6a390f3898e9139827a0cb33a90
brianayamamoto <36461368+brianayamamoto@users.noreply.github.com>
2020-04-17 21:43:29 -0400

    update org route

--- app.js
+++ app.js
@@ -65,7 +65,6 @@ app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 app.use('/organization', require('./routes/organization.js'));
 
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
